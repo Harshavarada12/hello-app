001005d61ae31290ed3315e4a9aa81b49c503d44
Harshavarada12 <sv9906@srmist.edu.in>
2026-04-21 14:52:59 +0530

    uc4 done

--- helloapp.java
+++ helloapp.java
@@ -1,7 +1,17 @@
-public class helloapp{
-    public static void main(String[] args){
-        // Get name from the command- line argument
-        String name = args[0];
-        System.out.println("Hello,"+name+"!");
+public class helloapp.java {
+    public static void main(String[] args) {
+        String message = "";
+        if(args.length == 0) {
+            message = "world";
+        }else{
+            for(int i =0; i < args.length;i++) {
+                message += args[i];
+            
+            if ( i < args.length-1){
+                message+=",";
+            }
+            }
+        }
+        System.out.println("hello,"+message + "!");
     }
 }
